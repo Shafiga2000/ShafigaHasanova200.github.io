ef5a75d9de1c1f077cc8afde538e2afd7fc281ef
ShafigaHasanova2000 <shasanova12147@ada.edu.az>
2022-12-01 22:08:14 +0400

    Create main.js

---
+++ hw2/main.js
@@ -0,0 +1,27 @@
+const url = 'https://api.openweathermap.org/data/2.5/';
+const key = '45f006a9f6651c2bd89393c3e1976b8c';
+
+const cityName = document.getElementById('cityName');
+const cityBtn = document.getElementById('cityBtn');
+const validationForName = document.querySelector('.validationForName')
+cityBtn.addEventListener('click', () => requestApiForCityName(cityName.value))
+
+
+function requestApiForCityName(name) {
+    if (name.trim().length != 0) {
+        fetch(`${url}weather?q=${name}&appid=${key}&units=metric&lang=en`)
+            .then(respons => respons.json())
+            .then(result => weatherDetailsForCity(result))
+    }
+    else {
+        validationForName.innerText = "Please write city name";
+    }
+}
+
+
+
+function weatherDetailsForCity(info) {
+    console.log(info.name);
+    console.log(Math.round(info.main.temp));
+}
+
