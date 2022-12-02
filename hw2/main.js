402f8a5602c684d29e701737cc8e002aab8714e8
ShafigaHasanova2000 <shasanova12147@ada.edu.az>
2022-12-02 21:21:53 +0400

    Update main.js

--- hw2/main.js
+++ hw2/main.js
@@ -14,7 +14,7 @@ let humidity = document.querySelector(".humidity");
 let windDirection = document.querySelector(".wind-direction");
 let responseTime = document.querySelector(".date-response")
 
-
+//City name  input
 const cityName = document.getElementById('cityName');
 const cityBtn = document.getElementById('cityBtn');
 
@@ -22,28 +22,177 @@ cityBtn.addEventListener('click', () => requestApiForCityName(cityName.value))
 
 
 function requestApiForCityName(name) {
-    if (name.trim().length != 0) {
+    if (name.trim() != "") {
         fetch(`${url}weather?q=${name}&appid=${key}&units=metric&lang=en`)
             .then(respons => respons.json())
-            .then(result => weatherDetailsForCity(result))
+            .then(result => {
+                if (result.cod == 200) {
+                    resetValidationForCityName();
+                    getCountryName(result)
+                    getResponseDate();
+                }
+                else {
+                    resetValidationForCityName();
+                    resetValues();
+                    getResponseDate();
+                    validationForName.innerText = "City name not found";
+                }
+            })
     }
     else {
+        resetValidationForCityName();
+        resetValues();
+        responseTime.innerText = "";
         validationForName.innerText = "Please write city name";
     }
+
+}
+
+function getCountryName(info) {
+    fetch(`https://restcountries.com/v3.1/alpha?codes=${info.sys.country}`)
+        .then(response => response.json())
+        .then(result => weatherDetailsForCity(result, info));
+
+}
+function weatherDetailsForCity(result, info) {
+    validationForName.innerText = "";
+
+    city.innerText = `${info.name} ,${result[0].name.common}`
+    temp.innerText = `${Math.round(info.main.temp)}°C`
+    condition.innerText = `${info.weather[0].main}`
+    description.innerText = `${info.weather[0].description}`
+    minmax.innerText = `${Math.round(info.main.temp_min)}°C / ${Math.round(info.main.temp_max)}°C`
+    realFeel.innerText = `${Math.round(info.main.feels_like)}°C`
+    pressure.innerText = `${info.main.pressure * 100} N/m2`
+    windSpeed.innerText = `${info.wind.speed} m/s`
+    humidity.innerText = `${info.main.humidity}%`
+    getDirection(info.wind.deg);
 }
 
 
 
-function weatherDetailsForCity(info) {
-    console.log(info.name);
-    console.log(Math.round(info.main.temp));
+//Lat and Lon intputs
+const corrBtn = document.getElementById('coordinantBtn');
+const lat = document.getElementById('lat')
+const lon = document.getElementById('lon')
+corrBtn.addEventListener('click', () => requestApiForCoordinant(lat.value, lon.value));
+
+function requestApiForCoordinant(value1, value2) {
+    if (value1.trim() != "" && value2.trim() != "") {
+        fetch(`${url}weather?lat=${value1}&lon=${value2}&appid=${key}&units=metric&lang=en`)
+            .then(respons => respons.json())
+            .then(result => {
+                if (result.cod == 200) {
+                    resetValidationForCorr();
+                    weatherDetailsForCorr(result)
+                    getResponseDate();
+                }
+                else {
+                    resetValues();
+                    resetValidationForCorr();
+                    getResponseDate();
+                    validationForCoor.innerText = "Coordinant not found"
+                }
+            })
+    }
+    else {
+        resetValues();
+        resetValidationForCorr();
+        validationForCoor.innerText = "Please write lat and lot"
+    }
+
 }
+
+
+function weatherDetailsForCorr(info) {
+    if (info.sys.country) {
+        fetch(`https://restcountries.com/v3.1/alpha?codes=${info.sys.country}`)
+            .then(response => response.json())
+            .then(result => getCountryNameForCorr(result, info));
+    }
+    else if (info.name != "") {
+        city.innerText = `${info.name},Country name not found`
+    }
+    else {
+        city.innerText = "City name not found ,Country name not found";
+    }
+    CoordinantAndCurrentInnerText(info)
+}
+function getCountryNameForCorr(result, info) {
+    city.innerText = `${info.name} , ${result[0].name.common}`
+}
+
+//Current cordiant button
+
+const locationBtn = document.getElementById('currentLoc');
+
+locationBtn.addEventListener('click', () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(onSuccess, onError)
+    }
+    else {
+        console.log("! ! !");
+    }
+})
+
+
+function onSuccess(position) {
+    const { latitude, longitude } = position.coords;
+    fetch(`${url}weather?lat=${latitude}&lon=${longitude}&appid=${key}&units=metric&lang=en`)
+        .then(respons => respons.json())
+        .then(result => weatherDetailsForCurrentPosition(result))
+    getResponseDate();
+}
+
+function onError(error) {
+    resetValidationForCurrLocation();
+    resetValues();
+    validationForCurrLocation.innerText = error.message;
+}
+
+function weatherDetailsForCurrentPosition(info) {
+    resetValidationForCurrLocation();
+    if (info.sys.country) {
+        fetch(`https://restcountries.com/v3.1/alpha?codes=${info.sys.country}`)
+            .then(response => response.json())
+            .then(result => getCountryNameForCurrentLocation(result, info));
+    }
+    else if (info.name != "") {
+        city.innerText = `${info.name},Country name not found`
+    }
+    else {
+        city.innerText = "City name not found ,Country name not found";
+    }
+    CoordinantAndCurrentInnerText(info);
+}
+function getCountryNameForCurrentLocation(result, info) {
+    city.innerText = `${info.name} ,${result[0].name.common}`
+}
+
+
 
 //Get response date
 function getResponseDate() {
     const utcStr = new Date();
     responseTime.innerText = utcStr.toLocaleTimeString();
 }
+
+//Coordinant and Current Cordinant values 
+function CoordinantAndCurrentInnerText(info) {
+    validationForCoor.innerText = "";
+    validationForCurrLocation.innerText = "";
+
+    temp.innerText = `${Math.round(info.main.temp)}°C`
+    condition.innerText = `${info.weather[0].main}`
+    description.innerText = `${info.weather[0].description}`
+    minmax.innerText = `${Math.round(info.main.temp_min)}°C / ${Math.round(info.main.temp_max)}°C`
+    realFeel.innerText = `${Math.round(info.main.feels_like)}°C`
+    pressure.innerText = `${info.main.pressure * 100} N/m2`
+    windSpeed.innerText = `${info.wind.speed} m/s`
+    humidity.innerText = `${info.main.humidity}%`
+    getDirection(info.wind.deg);
+}
+
 
 //Validations
 const validationForName = document.querySelector('.validationForName')
