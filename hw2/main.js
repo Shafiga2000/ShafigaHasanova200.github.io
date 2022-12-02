598b6350483052f7e1b54164f6e23ea30de5fdc2
ShafigaHasanova2000 <shasanova12147@ada.edu.az>
2022-12-02 13:44:38 +0400

    Update main.js

--- hw2/main.js
+++ hw2/main.js
@@ -3,7 +3,7 @@ const key = '45f006a9f6651c2bd89393c3e1976b8c';
 
 const cityName = document.getElementById('cityName');
 const cityBtn = document.getElementById('cityBtn');
-const validationForName = document.querySelector('.validationForName')
+
 cityBtn.addEventListener('click', () => requestApiForCityName(cityName.value))
 
 
@@ -23,5 +23,34 @@ function requestApiForCityName(name) {
 function weatherDetailsForCity(info) {
     console.log(info.name);
     console.log(Math.round(info.main.temp));
+}
+
+//Get response date
+function getResponseDate() {
+    const utcStr = new Date();
+    responseTime.innerText = utcStr.toLocaleTimeString();
+}
+const validationForName = document.querySelector('.validationForName')
+const validationForCoor = document.querySelector('.validationForCoor');
+const validationForCurrLocation = document.querySelector(".validationForCurrentLocation");
+//Validations
+function resetValidationForCityName() {
+    lat.value = "";
+    lon.value = "";
+    validationForCoor.innerText = "";
+    validationForCurrLocation.innerText = "";
+}
+function resetValidationForCurrLocation() {
+    lat.value = "";
+    lon.value = "";
+    cityName.value = "";
+    validationForCoor.innerText = "";
+    validationForName.innerText = "";
+}
+
+function resetValidationForCorr() {
+    cityName.value = '';
+    validationForName.innerText = "";
+    validationForCurrLocation.innerText = "";
 }
 
